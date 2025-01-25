f3b5ac1d47b66e00758480ca7ecd1d44fd8d9ba6
Phat Dang <encomos18021@gmail.com>
2025-01-25 16:45:54 -0500

    add chat area

---
+++ src/lib/MessageBubbleComponent.svelte
@@ -0,0 +1,46 @@
+<script>
+    export let content = "Error";
+    export let type;
+    export let color = "grey";
+    export let float = "left"
+
+    if (type == "ai") {
+        color = "#49cc9a";
+    }
+    else {
+        color = "#cdd1d0";
+        float = "right"
+    }
+</script>
+
+<div class="container">
+    <div class="bubble {type}" style="background-color: {color};">
+        {content}
+    </div>
+</div>
+
+
+<style>
+    .container {
+        display: flex;
+    }
+
+    .bubble.ai {
+        justify-content: flex-start; /* Align AI messages to the left */
+    }
+
+    .bubble.human {
+        background-color: black;
+        margin-left: auto;
+    }
+
+    .bubble {
+        color: white;
+        display: inline-block;; /* Keeps the bubble size fitting the text */
+        padding: 10px; /* Adds space around the text inside the bubble */
+        border-radius: 10px; /* Gives rounded corners */
+        max-width: 100%; /* Prevents bubbles from overflowing */
+        word-wrap: break-word; /* Breaks long words if necessary */
+        max-width: 40%;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,5 @@
 <script>
-    import ExampleComponent from "$lib/ExampleComponent.svelte";
-
+    import ChatAreaComponent from "$lib/ChatAreaComponent.svelte"
 </script>
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 
-<ExampleComponent name="test" />
+<ChatAreaComponent />
