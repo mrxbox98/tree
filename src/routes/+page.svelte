fee6f66e01b3c0431d24651ccd9f6164e4ef3fe5
mrxbox98 <29240392+mrxbox98@users.noreply.github.com>
2025-01-25 16:44:05 -0500

    work

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,39 @@
-<script>
-    import ExampleComponent from "$lib/ExampleComponent.svelte";
+<script lang="ts">
+    import { getAmazonPrices, getLatLong, getPlants, getZipCode } from "$lib/client";
+    import { onMount } from "svelte";
 
+    let zipCode = 0;
+
+    let plant = []
+
+    let priceData = [{}]
+
+    onMount(async () => {
+        let location = await getLatLong();
+
+        zipCode = await getZipCode(location.lat, location.long);
+
+        plant = await getPlants(zipCode);
+
+        priceData = await getAmazonPrices(plant + " seeds");
+    })
 </script>
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 
-<ExampleComponent name="test" />
+<div>
+    Your zip code is : {zipCode}
+</div>
+<div class="flex">
+    {#each plant as item}
+        <div>
+            <p>{item}</p>
+        </div>
+    {/each}
+</div>
+<div class="grid grid-cols-5">
+    {#each priceData as item}
+        <div>
+            <p>{item.title}</p>
+            <p>{item.price}</p>
+        </div>
+    {/each}
+</div>
