63c662ef4220aa963d6893bc4e1b638465614d73
mrxbox98 <29240392+mrxbox98@users.noreply.github.com>
2025-01-25 20:00:15 -0500

    c

--- src/routes/tests/+page.svelte
+++ src/routes/tests/+page.svelte
@@ -1,41 +1,5 @@
-<script lang="ts">
-    import { getAmazonPrices } from "$lib/client";
+<script>
+    import HoverFlower from "$lib/HoverFlower.svelte";
 
-    let mainel: HTMLElement;
-
-    let info: any = undefined
-
-    let loading = false;
-
-    async function onHover() {
-        loading = true
-
-        console.log("Hovered");
-
-        let amazonPrices = await getAmazonPrices(mainel.textContent+" seeds");
-
-        console.log(amazonPrices);
-
-        info = amazonPrices[0];
-
-        console.log(info);
-
-        box.style.top = `${mainel.getBoundingClientRect().top + mainel.getBoundingClientRect().height}px`;
-        box.style.left = `${mainel.getBoundingClientRect().left}px`;
-        box.style.display = "block";
-
-        loading = false
-    }
-
-    let box: HTMLElement;
 </script>
-
-<code bind:this={mainel} class="{loading ? 'cursor-wait' : ''}" onmouseover={onHover}>Rose</code>
-
-<div bind:this={box} class="w-[200px] absolute hidden p-2 rounded-lg shadow-lg shadow-gray-500/50">
-    {#if info}
-        <img src={info.img} alt="" />
-        <a href={info.link} target="_blank" class="hover:underline">{info.title}</a>
-        <p class="font-semibold text-right">{info.price}</p>
-    {/if}
-</div>
+<HoverFlower flower="Iris" />
