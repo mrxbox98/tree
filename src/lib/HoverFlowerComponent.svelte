38fa02d5a210aa0a09cbc2a5e71914ebcb4d119b
Phat Dang <encomos18021@gmail.com>
2025-01-25 19:59:37 -0500

    add plant hover

--- src/lib/HoverFlowerComponent.svelte
+++ src/lib/HoverFlowerComponent.svelte
@@ -1,20 +1,18 @@
 <script lang="ts">
     import { getAmazonPrices } from "$lib/client";
 
+    export let flower = "";
+    export let info: any = undefined
+
     let mainel: HTMLElement;
 
-    let info: any = undefined
+    
 
     let loading = false;
 
     async function onHover() {
         loading = true
-
-        console.log("Hovered");
-
         let amazonPrices = await getAmazonPrices(mainel.textContent+" seeds");
-
-        console.log(amazonPrices);
 
         info = amazonPrices[0];
 
@@ -28,14 +26,14 @@
     }
 
     let box: HTMLElement;
-</script> 
+</script>
 
-<code bind:this={mainel} class="{loading ? 'cursor-wait' : ''}" onmouseover={onHover}>Rose</code>
+<code bind:this={mainel} class="{loading ? 'cursor-wait' : ''}" onmouseover={onHover}>{flower}</code>
 
-<div bind:this={box} class="w-[200px] border border-green-500 absolute hidden p-2">
+<div bind:this={box} class="w-[200px] absolute hidden p-2 rounded-lg shadow-lg shadow-gray-500/50">
     {#if info}
         <img src={info.img} alt="" />
-        <p>{info.title}</p>
-        <p>{info.price}</p>
+        <a href={info.link} target="_blank" class="hover:underline">{info.title}</a>
+        <p class="font-semibold text-right">{info.price}</p>
     {/if}
 </div>
